e692372dab674f6341256de77859ec380d2f4f66
Artur Das <a.daschevici@gmail.com>
2024-05-25 22:08:07 +0200

    Add a basic crawler for loading page

--- src/product-crawl-basic/cmd/product-crawl-basic/main.go
+++ src/product-crawl-basic/cmd/product-crawl-basic/main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"time"
+
+	"github.com/chromedp/cdproto/dom"
+	"github.com/chromedp/chromedp"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	var html string
+	err := chromedp.Run(ctx,
+		// visit the target page
+		chromedp.Navigate("https://bot.sannysoft.com/"),
+		// wait for the page to load
+		chromedp.Sleep(2*time.Second),
+		// get the outer HTML of the page
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			node, err := dom.GetDocument().Do(ctx)
+			if err != nil {
+				return err
+			}
+			html, err = dom.GetOuterHTML().WithNodeID(node.NodeID).Do(ctx)
+			return err
+		}),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(html)
 }
