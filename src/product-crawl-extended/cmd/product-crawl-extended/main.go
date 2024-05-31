3a8aa7ed531d356608db7ad1ae45aad31e1ae8e4
Artur Das <a.daschevici@gmail.com>
2024-05-31 15:38:51 +0200

    Create a modular structure for the golang project

--- src/product-crawl-extended/cmd/product-crawl-extended/main.go
+++ src/product-crawl-extended/cmd/product-crawl-extended/main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"product-crawl-extended/internal/stage_one"
 )
 
 func main() {
-	var rootCmd = &cobra.Command{Use: "root"}
+	var rootCmd = &cobra.Command{Use: "spider"}
 	var cmd = &cobra.Command{
 		Use:   "cmd",
 		Short: "cmd short",
 		Long:  "cmd long",
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("cmd run")
+			stage_one.Init()
 		},
 	}
 	rootCmd.AddCommand(cmd)
---
+++ src/product-crawl-extended/internal/stage_one/stage_one.go
@@ -0,0 +1,9 @@
+package stage_one
+
+import "fmt"
+
+func Init() {
+	// This is a dummy function that does nothing.
+	// It's here to show that the stage_one package is being imported.
+	fmt.Println("stage_one imported")
+}
