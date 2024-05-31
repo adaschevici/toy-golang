cf6daa1ecd2cbc47ae784ffaf631e8df3c1b6a3d
Artur Das <a.daschevici@gmail.com>
2024-05-31 22:02:14 +0200

    Use commands and subcommands in cobra cli app

--- src/product-crawl-extended/cmd/product-crawl-extended/main.go
+++ src/product-crawl-extended/cmd/product-crawl-extended/main.go
@@ -8,16 +8,37 @@ import (
 
 func main() {
 	var rootCmd = &cobra.Command{Use: "spider"}
-	var cmd = &cobra.Command{
-		Use:   "crawl",
-		Short: "crawl basic set automation",
+	var cmd1 = &cobra.Command{
+		Use:   "crawl1",
+		Short: "crawl basic set automation1",
 		Long:  "This is the first crawl command and does very basic set automation.",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("cmd run")
+			fmt.Println("cmd run1")
 			fmt.Println("args", args)
 			stage_one.Crawl()
 		},
 	}
-	rootCmd.AddCommand(cmd)
+	var cmd2 = &cobra.Command{
+		Use:   "crawl2",
+		Short: "crawl basic set automation2",
+		Long:  "This is the first crawl command and doesbasic set automation.",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("cmd run2")
+			fmt.Println("args", args)
+			stage_one.Crawl()
+		},
+	}
+	var cmd3 = &cobra.Command{
+		Use:   "website",
+		Short: "crawl basic set automation3",
+		Long:  "This is the first crawl command and does very basic set a.",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("cmd runs")
+			fmt.Println("args", args)
+			stage_one.Crawl()
+		},
+	}
+	rootCmd.AddCommand(cmd1, cmd2)
+	cmd1.AddCommand(cmd3)
 	rootCmd.Execute()
 }
