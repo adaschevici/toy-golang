cb56d51b5cb130bb6b992c6ec9ea6023b76247c5
Artur Das <a.daschevici@gmail.com>
2024-05-31 22:22:06 +0200

    Add only basic automation to cli entrypoint

--- src/product-crawl-extended/cmd/product-crawl-extended/main.go
+++ src/product-crawl-extended/cmd/product-crawl-extended/main.go
@@ -3,42 +3,41 @@ package main
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"product-crawl-extended/internal/stage_one"
+	"log"
+	"product-crawl-extended/internal/first_automation"
 )
 
 func main() {
 	var rootCmd = &cobra.Command{Use: "spider"}
-	var cmd1 = &cobra.Command{
-		Use:   "crawl1",
+	var cmd = &cobra.Command{
+		Use:   "crawl",
 		Short: "crawl basic set automation1",
 		Long:  "This is the first crawl command and does very basic set automation.",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("cmd run1")
 			fmt.Println("args", args)
-			stage_one.Crawl()
 		},
 	}
-	var cmd2 = &cobra.Command{
-		Use:   "crawl2",
-		Short: "crawl basic set automation2",
-		Long:  "This is the first crawl command and doesbasic set automation.",
+	var basic = &cobra.Command{
+		Use:   "basic",
+		Short: "crawl basic set automation",
+		Long:  "This is the first crawl command and does basic set automation.",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("cmd run2")
-			fmt.Println("args", args)
-			stage_one.Crawl()
+			log.Println("cmd running basic automation")
+			log.Println("args", args)
+			first_automation.Crawl()
 		},
 	}
-	var cmd3 = &cobra.Command{
-		Use:   "website",
-		Short: "crawl basic set automation3",
-		Long:  "This is the first crawl command and does very basic set a.",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("cmd runs")
-			fmt.Println("args", args)
-			stage_one.Crawl()
-		},
-	}
-	rootCmd.AddCommand(cmd1, cmd2)
-	cmd1.AddCommand(cmd3)
+	// var cmd3 = &cobra.Command{
+	// 	Use:   "website",
+	// 	Short: "crawl basic set automation3",
+	// 	Long:  "This is the first crawl command and does very basic set a.",
+	// 	Run: func(cmd *cobra.Command, args []string) {
+	// 		fmt.Println("cmd runs")
+	// 		fmt.Println("args", args)
+	// 		stage_one.Crawl()
+	// 	},
+	// }
+	// rootCmd.AddCommand(cmd)
+	cmd.AddCommand(basic)
 	rootCmd.Execute()
 }
