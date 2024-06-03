24f1b9088480b7cc9d81ba8a8f6ad6bcec6782f0
Artur Das <a.daschevici@gmail.com>
2024-06-03 09:44:06 +0200

    Add a download page template

--- src/product-crawl-extended/fixtures/flasker.py
+++ src/product-crawl-extended/fixtures/flasker.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file, abort
 from time import sleep
 
 app = Flask(__name__)
@@ -8,6 +8,25 @@ app = Flask(__name__)
 def form():
     sleep(5)
     return render_template("form.html")
+
+
+@app.route("/download", methods=["GET"])
+def download():
+    return render_template("download.html")
+
+
+@app.route("/download/<filename>", methods=["GET"])
+def download_file(filename):
+    try:
+        import os
+
+        print(f"Downloading file: {filename}")
+        print(os.getcwd())
+        # Assuming the files to be downloaded are in the 'downloadables' directory
+        file_path = f"downloadables/{filename}"
+        return send_file(file_path, as_attachment=True)
+    except FileNotFoundError:
+        abort(404)
 
 
 @app.route("/data/", methods=["POST", "GET"])
