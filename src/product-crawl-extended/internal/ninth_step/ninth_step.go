70b216a392c04affa50af7e9913917c3611f6a8c
Artur Das <a.daschevici@gmail.com>
2024-06-04 15:10:52 +0200

    Add a snippet to do graceful shutdown of the browser so that it doesn't keep instances of chrome hanging around

--- src/product-crawl-extended/internal/ninth_step/ninth_step.go
+++ src/product-crawl-extended/internal/ninth_step/ninth_step.go
@@ -6,7 +6,7 @@ import (
 	"log"
 	// "time"
 
-	"github.com/chromedp/cdproto/dom"
+	// "github.com/chromedp/cdproto/dom"
 	"github.com/chromedp/chromedp"
 )
 
@@ -24,21 +24,27 @@ func Crawl() {
 	// to release the browser resources when
 	// it is no longer needed
 	defer cancel()
+	var injectedScript = `(function() {
+		let iframe = document.querySelector('div.challenge-stage iframe');
+		console.log('iframe: ', iframe);
+		return iframe;
+	})();`
 	var html string
 	if err := chromedp.Run(ctx,
 		chromedp.Navigate("https://www.g2.com/products/jira/reviews/"),
+		chromedp.Evaluate(injectedScript, &html),
 		// chromedp.Sleep(60*time.Second),
 		// we have an iframe, so we need to switch to it before we can interact with it
-		chromedp.WaitVisible(`label.cb-lb > input`),
-		chromedp.Click(`label.cb-lb > input`),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			rootNode, err := dom.GetDocument().Do(ctx)
-			if err != nil {
-				return err
-			}
-			html, err = dom.GetOuterHTML().WithNodeID(rootNode.NodeID).Do(ctx)
-			return err
-		}),
+		// chromedp.WaitVisible(`label.cb-lb > input`),
+		// chromedp.Click(`label.cb-lb > input`),
+		// chromedp.ActionFunc(func(ctx context.Context) error {
+		// 	rootNode, err := dom.GetDocument().Do(ctx)
+		// 	if err != nil {
+		// 		return err
+		// 	}
+		// 	html, err = dom.GetOuterHTML().WithNodeID(rootNode.NodeID).Do(ctx)
+		// 	return err
+		// }),
 	); err != nil {
 		log.Fatal("Error while trying to grab current ip.", err)
 		return
@@ -46,3 +52,45 @@ func Crawl() {
 
 	fmt.Println("HTML: ", html)
 }
+
+// snippet to allow Chrome to shutdown properly
+// ctx, cancel := context.WithTimeout(context.Background(), 300 * time.Second)
+// defer cancel()
+//
+// // create chrome instance
+// var c *chromedp.CDP
+// c, err := chromedp.New(ctx, chromeOption, chromedp.WithLog(log.Printf))
+//
+// // make sure Chrome is closed when exit
+// defer func() {
+// 	log.Printf("Shutdown chrome")
+// 	// shutdown chrome
+// 	err = c.Shutdown(ctx)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+//
+// 	// wait for chrome to finish
+// 	// Wait() will hang on Windows and Linux with Chrome headless mode
+// 	// we'll need to exit the program when this happens
+// 	ch := make(chan error)
+// 	go func() {
+// 		c.Wait()
+// 		ch <- nil
+// 	}()
+//
+// 	select {
+// 	case err = <-ch:
+// 		log.Println("chrome closed")
+// 	case <-time.After(10 * time.Second):
+// 		log.Println("chrome didn't shutdown within 10s")
+// 	}
+// }()
+//
+// err = taskFunc(ctx, c)
+// if err != nil {
+// 	// don't use Fatal because we need to deter functions to run
+// 	log.Printf("%v", err)
+// 	return
+// }
+//
